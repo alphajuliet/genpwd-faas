b127f62cfeb0f269ab19e4d08d17ac03462adff0
alphajuliet <andrew@alphajuliet.com>
2018-09-30 16:50:44 +1000

    Add options parameters to call. Release new version to production.

--- functions/__main__.js
+++ functions/__main__.js
@@ -2,11 +2,25 @@
 * GenPwd as a FaaS.
 * @param {Number} genId The generator to use.
 * @param {Number} nwords Number of words to generate.
+* @param {Number} punctuation Include punctuation in generated words.
+* @param {Number} capitals Include capitals.
+* @param {Number} numbers Include numbers.
 * @returns {Object}
 */
 
 const g = require('../src/genpwd'); 
 
-module.exports = (genId = 3, nwords = 10, context, callback) => {
-  callback(null, g.generate(genId = genId, nwords = nwords));
+module.exports = (
+  genId = 3, 
+  nwords = 10, 
+  punctuation = 0, 
+  capitals = 0, 
+  numbers = 0,
+  context, callback) => {
+    const opts = { 
+      punctuation: (punctuation == 1), 
+      capitals: (capitals == 1), 
+      numbers: (numbers == 1) 
+    };
+  callback(null, g.generate(genId = genId, nwords = nwords, options = opts));
 };
